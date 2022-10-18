c4a874a007b3a35917189402a3573b6769e685cf
edidk7 <d.ignacio292@gmail.com>
2022-10-18 18:17:49 -0300

    fix table page 10 - form validator

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,56 @@ const
     formularioUI = document.querySelector('#formulario'),
     listadoAutosUI = document.getElementById('listaActividades'),
     indexPageLinkUI = document.querySelector('.nav-link');
-    listadoPageLinkUI = document.querySelector('.list');
-let arrayAutos = [];
-
-const autos = [{
-    id: '0',
-    marca: 'Chevrolet',
-}]
+    listadoPageLinkUI = document.querySelector('.list'),
+    getUrl = window.location.pathname.split("/").slice(1).join("/"),
+    pageSize = 10;
+let arrayAutos = [],    
+    curPage = 1;
 
 
 // Funciones
+
+//Active Link Page Location
+if (getUrl === "index.html") {
+    indexPageLinkUI.classList.add("active");
+} else if (getUrl === "listado.html") {
+    indexPageLinkUI.classList.remove("active");
+    listadoPageLinkUI.classList.add("active");
+}
+
+const checkRut = (rut) => {
+    var valor = rut.value.replace('.','');
+    valor = valor.replace('-','');
+    
+    cuerpo = valor.slice(0,-1);
+    dv = valor.slice(-1).toUpperCase();
+    
+    rut.value = cuerpo + '-'+ dv
+    
+    if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
+    
+    suma = 0;
+    multiplo = 2;
+    
+    for(i=1;i<=cuerpo.length;i++) {
+    
+        index = multiplo * valor.charAt(cuerpo.length - i);
+        
+        suma = suma + index;
+        
+        if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+  
+    }
+    
+    dvEsperado = 11 - (suma % 11);
+    
+    dv = (dv == 'K')?10:dv;
+    dv = (dv == 0)?11:dv;
+    
+    if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+    
+    rut.setCustomValidity('');
+}
 
 const CrearItem = (nombre, rut, patente, marca, modelo, color) => {
 
@@ -50,7 +90,11 @@ const PintarDB = () => {
     if (arrayAutos == null) {
         arrayAutos = [];
     } else {
-        arrayAutos.forEach(element => {
+        arrayAutos.filter((row, index) => {
+            let start = (curPage - 1) * pageSize;
+            let end = curPage * pageSize;
+            if (index >= start && index < end) return true;
+        }).forEach(element => {
             listadoAutosUI.innerHTML += `
             <tr>       
                 <td>${element.nombre}</td>
@@ -67,7 +111,6 @@ const PintarDB = () => {
     }
     console.log(arrayAutos);
 }
-
 
 const EliminarDB = (id) => {
 
@@ -97,12 +140,11 @@ formularioUI.addEventListener('submit', (e) => {
         colorUI = document.querySelector('#color').value;
 
     CrearItem(nombreUI, rutUI, patenteUI, marcaUI, modeloUI, colorUI);
-    
+
     GuardarDB();
 
     formularioUI.reset();
 });
-
 
 
 document.addEventListener('DOMContentLoaded', PintarDB);
@@ -121,11 +163,4 @@ listadoAutosUI.addEventListener("click", (e) => {
     }
 })
 
-//Active Link Page Location
-const getUrl = window.location.pathname.split("/").slice(1).join("/");
-if( getUrl === "index.html"){
-    indexPageLinkUI.classList.add("active");
-} else if(getUrl === "listado.html"){
-    indexPageLinkUI.classList.remove("active");
-    listadoPageLinkUI.classList.add("active");
-}
+
